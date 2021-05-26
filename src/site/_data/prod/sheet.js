8f7ce394b275bf198225811acd09a34dea536c8b
Jono <work@jrkd.co.uk>
2021-05-26 17:13:30 +1200

    Mostly updated sheets.js so we can feed in an unlimited number of sheets - these sheets can be col 1 = key, col 2 = value, in which case you can access each value via sheet.sheetName.key - or as a list of objects based on row 0 header names for property names eg. sheet.sheetName[0].col1HeaderName
    NB: We need to have sheet names that are okay as JSON keys.

--- src/site/_data/prod/sheet.js
+++ src/site/_data/prod/sheet.js
@@ -1,63 +1,57 @@
-const axios  = require('axios');
 const seed   = require('../../../utils/save-seed.js');
-
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 // Once a googel sheet is "published to the web" we can access its JSON
 // via a URL of this form. We just need to pass in the ID of the sheet
 // which we can find in the URL of the document.
-const sheetID = "1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0";
-const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
+const sheetID = "15BINVTpoaIZsUgiLUgQRwWFH73pWCd2HNJWO0B0GtMI";
+const API_KEY = "AIzaSyAwhZxE7CORL6J-UMH226f9fWa1i-zvJzQ";
+
+process.on("unhandledRejection", (error) => {
+  console.error("unhandledd", error); // This prints error with stack included (as for normal errors)
+  throw error; // Following best practices re-throw error and let the process exit with error code
+});
 
 module.exports = () => {
-  return new Promise((resolve, reject) => {
-
-    console.log(`Requesting data from ${googleSheetUrl}`);
-
-    axios.get(googleSheetUrl)
-      .then(response => {
-        // massage the data from the Google Sheets API into
-        // a shape that will more convenient for us in our SSG.
-        var data = {
-          "content": []
-        };
-        response.data.feed.entry.forEach(item => {
-          data.content.push({
-            "header": item.gsx$header.$t,
-            "header2": item.gsx$header2.$t,
-            "body": item.gsx$body.$t,
-            "body2": item.gsx$body2.$t,
-            "body3":  item.gsx$body3.$t,
-            "body4": item.gsx$body4.$t,
-            "body5": item.gsx$body5.$t,
-            "body6":  item.gsx$body6.$t,
-            "body7": item.gsx$body7.$t,
-            "body8": item.gsx$body8.$t,
-            "body9":  item.gsx$body9.$t,
-            "body10": item.gsx$body10.$t,
-            "body11": item.gsx$body11.$t,
-            "body12":  item.gsx$body12.$t,
-            "body13": item.gsx$body13.$t,
-            "body14": item.gsx$body14.$t,
-            "body15":  item.gsx$body15.$t,
-            "body16": item.gsx$body16.$t,
-            "body17": item.gsx$body17.$t,
-            
-          })
+  return new Promise(async (resolve, reject) => {
+    let ourJSON = {};
+    const doc = new GoogleSpreadsheet(sheetID);
+    
+    doc.useApiKey(API_KEY);
+    await doc.loadInfo();
+    for (let index = 0; index < doc.sheetCount; index++) {
+      const sheet = doc.sheetsByIndex[index];
+      await sheet.loadCells();
+      const rows = await sheet.getRows();
+      
+      if(sheet.headerValues[0] == "key"){ //we have key/value list.
+        ourJSON[sheet.title] = {};
+        rows.forEach((row, _index)=>{
+          ourJSON[sheet.title][row.key] = row.value;
         });
+      } 
+      else //We have a list of objects dont we now?
+      { 
+        ourJSON[sheet.title] = rows.map((row)=>{
+          if(sheet.headerValues.length == 1)
+          {
+            return row[sheet.headerValues[0]];
+          }
+          else
+          {
+            let rowObj = {};
+            sheet.headerValues.forEach((headerValue, _index)=>{
+              rowObj[headerValue] = row[headerValue];
+            })
+            return rowObj;
+          }
+        }); 
+      }
+    }
 
-        // stash the data locally for developing without
-        // needing to hit the API each time.
-        seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
-
-        // resolve the promise and return the data
-        resolve(data);
-
-      })
-
-      // uh-oh. Handle any errrors we might encounter
-      .catch(error => {
-        console.log('Error :', error);
-        reject(error);
-      });
+    // stash the data locally for developing without
+    // needing to hit the API each time.
+    console.log(ourJSON);
+    seed(JSON.stringify(ourJSON), `${__dirname}/../dev/sheet.json`);
   })
 }
--- src/site/index.md
+++ src/site/index.md
@@ -7,18 +7,9 @@ layout: default
 
 
 <div class="listing">
-{%- for item in sheet.content -%}
-  <h1>{{ item.header }} </h1>
-{%- endfor -%}
+  <h1>{{ sheet.layout.email }} </h1>
+
 </div>
-
-
-<div class="listing">
-{%- for item in sheet.content -%}
-  <h4>{{ item.header2}} {{ item.body2 }}  </h4>
-{%- endfor -%}
-</div>
-
 
 
 
@@ -26,18 +17,12 @@ layout: default
 
 
 <div class="listing">
-{%- for item in sheet.content -%}
-  <h4>{{ item.body10 }} {{ item.body14 }}  </h4>
+<h2>About images 1</h2>
+{%- for item in sheet.aboutimages1 -%}
+  <h4>{{ item.title }}</h4>
+  <h4>{{ item.imageLinkURL }}</h4>
 {%- endfor -%}
 </div>
-<ul class="listing">
-{%- for item in sheet.content -%}
-  <li> {{ item.body3 }}  </li>
-  <li>{{ item.body4}}  </li>
-  <li> {{ item.body5 }}  </li>
-  <li>{{ item.body6 }}  </li>
-{%- endfor -%}
-</ul>
 
 ## Clone here!!
 
